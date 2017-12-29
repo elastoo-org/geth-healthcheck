b15a956c42f126ba215e9cea246ebcea0641b896
Dmitry S. Vlasov <z1nkum@gmail.com>
2017-12-29 03:06:10 +0300

    add env handlers

--- ethereum-hc.py
+++ ethereum-hc.py
@@ -1,14 +1,16 @@
 import BaseHTTPServer
 import httplib
 import json
+import os
 
-HC_HOST = "127.0.0.1"
-HC_PORT = 8082
+HC_HOST = os.getenv('HC_HOST', "127.0.0.1")
+HC_PORT = os.getenv('HC_PORT', 8082)
 
-RPC_HOST = "127.0.0.1"
-RPC_PORT = "8545"
-RPC_TIMEOUT = 2
-FAIL_CODE = 500
+RPC_HOST = os.getenv('RPC_HOST', '127.0.0.1')
+RPC_PORT = os.getenv('RPC_PORT', '8545')
+
+RPC_TIMEOUT = os.getenv('RPC_TIMEOUT', '2')
+FAIL_CODE = os.getenv('RPC_FAIL_CODE', 500)
 
 
 class CheckHealth:
